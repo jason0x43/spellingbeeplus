b178a47b74838bec2e1cb21503853732e45b0d06
Jason Cheatham <jason@jasoncheatham.com>
2024-05-21 21:55:50 -0600

    refactor: simplify popup

--- extension/Shared (Extension)/Resources/popup.js
+++ extension/Shared (Extension)/Resources/popup.js
@@ -1,8 +1,5 @@
 import { getNativeInfo } from "./info.js";
 
-/** @type {string | undefined} */
-let status = undefined;
-
 /**
  * @param {string} selector
  * @param {string} text
@@ -14,51 +11,5 @@ function setElemText(selector, text) {
 	}
 }
 
-function updateStatus() {
-	browser.runtime
-		.sendMessage({ type: "getStatus" })
-		.then((newStatus) => {
-			if (newStatus !== status) {
-				if (newStatus) {
-					setElemText("#status", newStatus);
-					log(`Set status to ${newStatus}`);
-				} else {
-					setElemText("#status", "Unavailable");
-					log("Waiting for status...");
-				}
-				status = newStatus;
-			}
-		})
-		.catch((error) => {
-			log(`Error getting status: ${error}`);
-		});
-}
-
-/**
- * @param {string} message
- */
-function log(message) {
-	const log = document.querySelector("#log");
-	const entry = document.createElement("p");
-	entry.textContent = message;
-	log?.append(entry);
-}
-
-log("Starting up...");
-
-await new Promise((resolve) => setTimeout(resolve, 1000));
-
-setInterval(() => {
-	updateStatus();
-}, 1000);
-
-updateStatus();
-
 const version = await getNativeInfo("version", "getVersion");
 setElemText("#version", version);
-log(`Set version to ${version}`);
-
-const config = await getNativeInfo("config", "getConfig");
-setElemText("#host", config?.apiHost);
-log("Loaded config");
-log(`Using API host ${config?.apiHost}`);
